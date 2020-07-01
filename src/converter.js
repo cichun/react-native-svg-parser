8ccb0b85506cbfeb8054565c6ccdee89334f0b1b
Robert Maciaszczyk <r.maciaszczyk@myaic.com>
2020-07-01 09:43:07 +0200

    clicking svg - stable ver

--- src/converter.js
+++ src/converter.js
@@ -166,92 +166,89 @@ function findProperty (markup, propertyName) {
   return id && id.value
 }
 
-function traverse (markup, config, i = 0, onPress, colorsMap) {
-
-  if (!markup || !markup.nodeName || !markup.tagName) {
-    return null
-  }
-  const tagName = markup.nodeName
-  const idName = findId(markup)
-  if (idName && config.omitById && config.omitById.includes(idName)) {
-    return null
+class Traverse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.elementRef = React.createRef();
   }
 
-  const elementRef = React.createRef();
+  render() {
+    const {markup, config, i = 0, onPress, colorsMap} = this.props;
 
-
-  let attrs = []
-  if (tagName === 'svg') {
-    const viewBox = extractViewbox(markup)
-    attrs.push({
-      name: 'width',
-      value: config.width || viewBox.width
-    })
-    attrs.push({
-      name: 'height',
-      value: config.height || viewBox.height
-    })
-    attrs.push({
-      name: 'viewBox',
-      value: config.viewBox || viewBox.viewBox || '0 0 50 50'
-    })
-  } else if (tagName === 'g') {
-    // if(idName) {
-    //   attrs.push({
-    //     name: 'onPress',
-    //     value: () => onPress(idName, elementRef)
-    //   })
-    // }
-  } else {
-    // otherwise, if not SVG, check to see if there is CSS to apply.
-    // const cssPropsResult = { cssProps:[], attrs:[] };//findApplicableCssProps(markup, config)
-    const cssPropsResult = findApplicableCssProps(markup, config)
-    const additionalProps = addNonCssAttributes(markup, cssPropsResult)
-    const className = findProperty(markup,'class')
-
-    if(idName && className && className.includes('circle')) {
-      additionalProps.push({
-        name: 'onPress',
-        value: () => onPress(idName, elementRef)
-      })
+    if (!markup || !markup.nodeName || !markup.tagName) {
+      return null
     }
-    // const fill = findFill(markup)
-    const forcedColor = colorsMap.has(idName) ? colorsMap.get(idName):null
-    if(forcedColor) {
-      additionalProps.push({
-        name: 'fill',
-        value: forcedColor ? forcedColor : fill
-      })
+    const tagName = markup.nodeName
+    const idName = findId(markup)
+    if (idName && config.omitById && config.omitById.includes(idName)) {
+      return null
     }
 
+    let attrs = []
+    if (tagName === 'svg') {
+      const viewBox = extractViewbox(markup)
+      attrs.push({
+        name: 'width',
+        value: config.width || viewBox.width
+      })
+      attrs.push({
+        name: 'height',
+        value: config.height || viewBox.height
+      })
+      attrs.push({
+        name: 'viewBox',
+        value: config.viewBox || viewBox.viewBox || '0 0 50 50'
+      })
+    } else if (tagName === 'g') {
+      // if(idName) {
+      //   attrs.push({
+      //     name: 'onPress',
+      //     value: () => onPress(idName, elementRef)
+      //   })
+      // }
+    } else {
+      // otherwise, if not SVG, check to see if there is CSS to apply.
+      const cssPropsResult = findApplicableCssProps(markup, config)
+      const additionalProps = addNonCssAttributes(markup, cssPropsResult)
+      const className = findProperty(markup, 'class')
 
-    // add to the known list of total attributes.
-    attrs = [...attrs, ...cssPropsResult.attrs, ...additionalProps]
+      // if(idName && className && className.includes('circle')) {
+      if (idName && className && className.includes('circle')) {
+        additionalProps.push({
+          name: 'onPress',
+          value: () => onPress(idName, this.elementRef)
+        })
+      }
+
+      // add to the known list of total attributes.
+      attrs = [...attrs, ...cssPropsResult.attrs, ...additionalProps]
+    }
+
+    // map the tag to an element.
+    const Elem = mapping[tagName.toLowerCase()]
+
+    // Note, if the element is not found it was not in the mapping.
+    if (!Elem) {
+      return null
+    }
+
+    const children = (Elem === Text && markup.childNodes.length === 1)
+        ? markup.childNodes[0].data
+        : markup.childNodes.length ? Object.values(markup.childNodes).map((child) => {
+          // return traverse(child, config, ++i, onPress, colorsMap)
+          return <Traverse markup={child} config={config} i={i+1} key={i+1+Math.random()} onPress={onPress} colorsMap={colorsMap}/>
+        }).filter((node) => {
+          return !!node
+        }) : []
+
+    const elemAttributes = {}
+    attrs.forEach((attr) => {
+      elemAttributes[attr.name] = attr.value
+    })
+
+    const k = i + Math.random()
+    return <Elem ref={this.elementRef} testProp={1} {...elemAttributes} key={k}>{children}</Elem>
   }
-
-  // map the tag to an element.
-  const Elem = mapping[ tagName.toLowerCase() ]
-
-  // Note, if the element is not found it was not in the mapping.
-  if (!Elem) {
-    return null
-  }
-
-  const children = (Elem === Text && markup.childNodes.length === 1)
-    ? markup.childNodes[0].data
-    : markup.childNodes.length ? Object.values(markup.childNodes).map((child) => {
-      return traverse(child, config, ++i, onPress, colorsMap)
-    }).filter((node) => {
-      return !!node
-    }) : []
-
-  const elemAttributes = {}
-  attrs.forEach((attr) => {
-    elemAttributes[attr.name] = attr.value
-  })
-
-  const k = i + Math.random()
-  return <Elem ref={elementRef} {...elemAttributes} key={k}>{ children }</Elem>
 }
 
 export { extractViewbox, getCssRulesForAttr, findApplicableCssProps, addNonCssAttributes }
@@ -260,26 +257,6 @@ export default (dom, cssAst, config, onPress,colorsMap) => {
   config = Object.assign({}, config, {
     cssRules: (cssAst && cssAst.stylesheet && cssAst.stylesheet.rules) || []
   })
-  return traverse(dom.documentElement, config, 0,onPress,colorsMap)
+  return <Traverse markup={dom.documentElement} config={config} i={0} onPress={onPress} colorsMap={colorsMap}/>
+  // return traverse(dom.documentElement, config, 0,onPress,colorsMap)
 }
-
-
-// export default (dom, cssAst, config, onPress, colors) => {
-// const Converter = ({dom, cssAst, config, onPress, colors}) => {
-// const Konverter = (dom, cssAst, config, onPress, colors) => {
-//   config = Object.assign({}, config, {
-//     cssRules: (cssAst && cssAst.stylesheet && cssAst.stylesheet.rules) || []
-//   })
-//   return traverse(dom.documentElement, config, 0,onPress, colors)
-// }
-
-
-
-// const mapStateToProps = (state) => {
-//   const { colors } = state.SvgModuleReducer;
-//   return { colors };
-// };
-// const mapDispatchToProps = (dispatch) =>
-//     bindActionCreators({ clickElement }, dispatch);
-// export default connect(mapStateToProps, {})(Konverter);
-// export default Converter;
