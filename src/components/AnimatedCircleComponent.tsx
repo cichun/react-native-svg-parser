26452dcfb308be175e8ac3221138a187289b35c9
Robert Maciaszczyk <r.maciaszczyk@myaic.com>
2020-09-07 16:23:42 +0200

    adjust to new reanimated lib

--- src/components/AnimatedCircleComponent.tsx
+++ src/components/AnimatedCircleComponent.tsx
@@ -1,6 +1,6 @@
-import Svg, {Circle} from "react-native-svg";
-import {Animated, Easing, Dimensions, View} from "react-native";
-import React from "react";
+import Svg, { Circle } from 'react-native-svg';
+import { Animated, Easing, Dimensions, View } from 'react-native';
+import React from 'react';
 import extractBrush from 'react-native-svg/lib/module/lib/extract/extractBrush';
 
 // https://www.decoide.org/react-native/docs/animated.html
@@ -14,23 +14,23 @@ export default class AnimatedCircleComponent extends Circle {
         super(props);
         this.state = {
             circleRadius: new Animated.Value(props.r),
-            fillValue: new Animated.Value(0),
-        }
+            fillValue: new Animated.Value(0)
+        };
 
         this.state.circleRadius.addListener((circleRadius) => {
-            this._myCircle.setNativeProps({r: circleRadius.value.toString()});
+            this._myCircle && this._myCircle.setNativeProps({ r: circleRadius.value.toString() });
         });
 
 
         this.state.fillValue.addListener(() => {
-            const {fill} = this.props;
-            const {fillValue} = this.state;
+            const { fill } = this.props;
+            const { fillValue } = this.state;
             const color = fillValue.interpolate({
                 inputRange: [0, 1],
-                outputRange: ['#ff000090', 'white'],
+                outputRange: ['#ff000090', 'white']
             });
             this._myCircle.setNativeProps({
-                fill: extractBrush(color.__getAnimatedValue()),
+                fill: extractBrush(color.__getAnimatedValue())
                 // stroke: extractBrush(color.__getAnimatedValue()),
                 // strokeWidth: opac.__getAnimatedValue().toString()
             });
@@ -38,33 +38,31 @@ export default class AnimatedCircleComponent extends Circle {
 
 
         setTimeout(() => {
-            Animated.spring(this.state.circleRadius, {toValue: this.props.r * 1, friction: 3}).start();
-        }, 2000)
+            Animated.spring(this.state.circleRadius, {
+                toValue: this.props.r * 1,
+                friction: 3,
+                useNativeDriver: true
+            }).start();
+        }, 2000);
 
     }
 
     setNativeProps = (props) => {
         this._myCircle.setNativeProps(props);
         this.animateMyCircle();
-    }
+    };
 
     animateMyCircle = () => {
-        // Animated.spring(this.state.circleRadius, {
-        //     toValue: (50),
-        //     friction: 3
-        // }).start()
         Animated.sequence([
-            // Animated.timing(this.state.circleRadius, {
-            //     toValue: (this.props.r*2),
-            //     easing: Easing.elastic,
-            // }),
             Animated.parallel([
                 Animated.timing(this.state.circleRadius, {
                     toValue: (this.props.r * 2),
                     easing: Easing.bounce,
+                    useNativeDriver: true
                 }),
                 Animated.timing(this.state.fillValue, {
                     toValue: 0,
+                    useNativeDriver:true
                     // duration: 500,
                     // easing: Easing.bounce
                 })
@@ -73,16 +71,18 @@ export default class AnimatedCircleComponent extends Circle {
             Animated.parallel([
                 Animated.timing(this.state.circleRadius, {
                     toValue: (this.props.r),
-                    easing: Easing.bounce
+                    easing: Easing.bounce,
+                    useNativeDriver:true
                 }),
                 Animated.timing(this.state.fillValue, {
                     toValue: 1,
+                    useNativeDriver:true
                     // duration: 500,
                     // easing: Easing.bounce
                 })
             ])
 
-        ]).start(({finished}) => {
+        ]).start(({ finished }) => {
             if (finished) {
                 // this.state.fillValue.setValue(0);
             }
